903c6e8013000a68e9cd6df544b7621314a6f665
Dan Manning <dmanning23@hotmail.com>
2015-12-20 16:42:00 -0600

    update externs

--- Windows/Program.cs
+++ Windows/Program.cs
@@ -1,16 +1,14 @@
-using System;
-
 namespace InputWrapperSample
 {
 #if WINDOWS || XBOX
-    static class Program
+	static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (var game = new Game1())
             {
                 game.Run();
             }
